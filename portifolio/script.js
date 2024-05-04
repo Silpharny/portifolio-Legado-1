958d10e786daf88ff1845ea62aec7a67d572cd9b
Silpharny <silpharny@gmail.com>
2024-05-04 18:16:38 -0300

    Portifólio

---
+++ portifolio/script.js
@@ -0,0 +1,7 @@
+/*
+const inicioNav = document.querySelector("#inicio")
+const sobreNav = document.querySelector("#sobre")
+const projetosNav = document.querySelector("#projetos")
+const contatoNav = document.querySelector("#contato")
+const navigation = document.querySelectorAll(".navigation")
+*/  
