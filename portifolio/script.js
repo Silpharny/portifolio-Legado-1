4e8914067cfbfb3d25dd36a9a31066b97b4950a7
Silpharny <silpharny@gmail.com>
2024-05-05 19:07:49 -0300

    Reorganizando secs

--- portifolio/script.js
+++ portifolio/script.js
@@ -4,4 +4,4 @@ const sobreNav = document.querySelector("#sobre")
 const projetosNav = document.querySelector("#projetos")
 const contatoNav = document.querySelector("#contato")
 const navigation = document.querySelectorAll(".navigation")
-*/  
+*/
